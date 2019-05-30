41b9095d10f8ff6d67851511da71005836379de3
Litterial <yungice12@gmail.com>
2019-05-30 11:04:58 -0500

    endpoints for commenting on questions and answers

--- passionProjectApp/urls.py
+++ passionProjectApp/urls.py
@@ -6,15 +6,25 @@ urlpatterns=[
     path('register/',views.register,name='register'),
     path('registerpass/',views.registerPass,name='registerpass'),
 
+    path('ask/read/<int:ID>/',views.ask_read,name='ask_read'),
+
     path("ask/",views.ask,name='ask'),
-    path("ask/edit/",views.edit_ask,name="edit_ask"),
-    path("ask/delete/",views.del_ask,name='del_ask'),
+    path("ask/edit/",views.ask_edit,name="ans_edit"),
+    path("ask/delete/",views.ask_del,name='ask_del'),
 
 
 
     path("answer/<int:ID>/",views.answer,name='answer'),
-    path('answer/edit/',views.edit_ans,name="edit_ans"),
-    path('answer/delete/',views.del_ans,name="del_ans"),
+    path('answer/edit/',views.answer_edit,name="answer_edit"),
+    path('answer/delete/',views.answer_del,name="answer_del"),
+
+    path('ask/comment/<int:ID>/',views.comment_ask,name='comment_ask'),
+    path('ask/comment/<int:ID>/edit',views.comment_answer_edit,name='comment_answer_edit'),
+    path('ask/comment/<int:ID>/delete',views.comment_answer_del,name='comment_answer_del'),
+
+    path('answer/comment/<int:ID>/',views.comment_answer,name='comment_answer'),
+    path('answer/comment/<int:ID>/edit',views.comment_answer_edit,name='comment_answer_edit'),
+    path('answer/comment/<int:ID>/delete',views.comment_answer_del,name='comment_answer_del'),
 
 
 
--- passionProjectApp/views.py
+++ passionProjectApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,redirect,get_object_or_404
-from .forms import RealQuestionForm,RealQuestion,UserForm,AnswerForm,Answer
+from .forms import RealQuestionForm,RealQuestion,UserForm,AnswerForm,Answer,RealQuestionComment,AnswerComment
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
@@ -9,19 +9,6 @@ def index(request):
     allquestions=RealQuestion.objects.all()
     context={"allquestions":allquestions}
     return render(request,"passionProjectApp/index.html",context)
-@login_required
-def ask(request):
-    form=RealQuestionForm(request.POST or None)
-    if request.method == 'POST':
-        if form.is_valid():
-            RealQuestion.objects.create(title=request.POST['title'],question=request.POST['question'],author=request.user)
-            return redirect('index')
-        else:
-            form=RealQuestionForm(request.POST or None)
-            context={'form':form,'errors':form.errors}
-            return render(request,'passionProjectApp/ask.html',context)
-    return render(request,"passionProjectApp/ask.html",{"form":form})
-
 def register(request):
     form=UserForm(request.POST or None)
     print('here')
@@ -34,13 +21,37 @@ def register(request):
             context={'form':form,'errors':form.errors}
             return render(request,"passionProjectApp/register.html",context)
     return render(request,"passionProjectApp/register.html",{"form":form})
-
 def registerPass(request):
     return render(request,"passionProjectApp/registerPass.html")
-def edit_ask(request):
-    return render(request,"passionProjectApp/edit_ask.html",)
-def del_ask(request):
-    return render(request,"passionProjectApp/del_ask.html")
+
+
+
+@login_required
+def ask(request):
+    form=RealQuestionForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            RealQuestion.objects.create(title=request.POST['title'],question=request.POST['question'],author=request.user)
+            return redirect('index')
+        else:
+            form=RealQuestionForm(request.POST or None)
+            context={'form':form,'errors':form.errors}
+            return render(request,'passionProjectApp/ask.html',context)
+    return render(request,"passionProjectApp/ask.html",{"form":form})
+def ask_edit(request):
+    return render(request,"passionProjectApp/ask_edit.html",)
+def ask_del(request):
+    return render(request,"passionProjectApp/ask_del.html")
+def ask_read(request,ID):
+    question=get_object_or_404(RealQuestion,pk=ID)
+    print(question)
+    print(type(question))
+    allAnswers=Answer.objects.filter(parent=ID)
+    allQuestionComments=RealQuestionComment.objects.filter(parent=ID)
+    allAnswerComments=AnswerComment.objects.filter(parent_id=ID)
+    print(allAnswers)
+    context={"question":question,"answers":allAnswers,"question_comment":allQuestionComments,"answer_comment":allAnswerComments}
+    return render(request,"passionProjectApp/ask_read.html",context)
 
 
 @login_required
@@ -56,9 +67,27 @@ def answer(request,ID):
             context={'form':form,'errors':form.errors}
             return render(request,"passionProjectApp/answer.html",context)
     return render(request,"passionProjectApp/answer.html",{"form":form})
-def edit_ans(request):
-    return render(request,"passionProjectApp/edit_ans.html")
-def del_ans(request):
-    return render(request,"passionProjectApp/del_ans.html")
+def answer_edit(request):
+    return render(request,"passionProjectApp/answer_edit.html")
+def answer_del(request):
+    return render(request,"passionProjectApp/answer_del.html")
+
+@login_required
+def comment_ask(request):
+    return render(request,'passionProjectApp/comment_ask.html')
+def comment_ask_edit(request):
+    return render(request,"passionProjectApp/comment_ask_edit.html")
+def comment_ask_del(request):
+    return render(request,"passionProjectApp/comment_ask_del.html")
+
+@login_required
+def comment_answer(request):
+    return render(request,'passionProjectApp/comment_answer.html')
+def comment_answer_edit(request):
+    return render(request,"passionProjectApp/comment_answer_edit.html")
+def comment_answer_del(request):
+    return render(request,"passionProjectApp/comment_answer_del.html")
+
+
 def search (request):
     return render(request,"passionProjectApp/search.html")
