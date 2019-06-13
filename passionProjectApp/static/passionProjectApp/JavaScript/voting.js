b009415b28554b9c5d68b1ebdd39c7a8f9699fd2
Litterial <yungice12@gmail.com>
2019-06-13 08:44:23 -0500

    took out upvoting in case it breaks on my server

--- passionProjectApp/static/passionProjectApp/JavaScript/voting.js
+++ passionProjectApp/static/passionProjectApp/JavaScript/voting.js
@@ -89,3 +89,83 @@ $(".Q-downvote-link ").click(function (e) {
         }
     )
 });
+
+$(".A-upvote-link ").click(function (e) {
+    console.log('test');
+    e.preventDefault();
+    console.log('preventDefault works');
+    var dataUrl = $(this).attr('data-href');
+    var tempID=("#answer"+($(this).attr("data-upvote")));
+    var questionID=document.querySelector(tempID);
+    console.log(questionID);
+    $.ajax({
+            url: dataUrl,
+            method: "GET",
+            data: {},
+            success: function (data) {
+                console.log(data);
+                var newLikes;
+                if(data.upvote){
+                    console.log(data.upvote);
+                    newLikes=data.voteTotal;
+                    console.log('upvote');
+                    console.log(newLikes);
+                    updateText(questionID,newLikes);
+
+                }
+                else{
+                    newLikes=data.voteTotal;
+                    console.log(data.upvote);
+                    console.log('downvote');
+                    console.log(newLikes);
+                    updateText(questionID,newLikes);
+                }
+
+            },
+            error: function (fail) {
+                console.log('fail');
+                console.log(fail)
+            }
+        }
+    )
+});
+
+$(".A-downvote-link ").click(function (e) {
+    console.log('test');
+    e.preventDefault();
+    console.log('preventDefault works');
+    var dataUrl = $(this).attr('data-href');
+    var tempID=("#answer"+($(this).attr("data-downvote")));
+    var questionID=document.querySelector(tempID);
+    console.log(questionID);
+    $.ajax({
+            url: dataUrl,
+            method: "GET",
+            data: {},
+            success: function (data) {
+                console.log(data);
+                var newLikes;
+                if(data.downvote){
+                    console.log(data.downvote);
+                    newLikes=data.voteTotal;
+                    console.log('upvote');
+                    console.log(newLikes);
+                    updateText(questionID,newLikes);
+
+                }
+                else{
+                    newLikes=data.voteTotal;
+                    console.log(data.downvote);
+                    console.log('downvote');
+                    console.log(newLikes);
+                    updateText(questionID,newLikes);
+                }
+
+            },
+            error: function (fail) {
+                console.log('fail');
+                console.log(fail)
+            }
+        }
+    )
+});
